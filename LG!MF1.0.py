a04df8ec8c157fe519f667f21722698b083860d3
catsanzsh <84363801+catsanzsh@users.noreply.github.com>
2024-12-19 12:48:36 -0800

    Create LG!MF1.0.py

---
+++ LG!MF1.0.py
@@ -0,0 +1,190 @@
+import pygame
+import random
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TILE_SIZE = 40
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BRICK_COLOR = (200, 100, 100)
+
+# Player properties
+PLAYER_WIDTH = 32
+PLAYER_HEIGHT = 32
+PLAYER_SPEED = 5
+JUMP_FORCE = -15
+GRAVITY = 0.8
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, color):
+        super().__init__()
+        self.image = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.on_ground = False
+
+    def update(self):
+        self.velocity_y += GRAVITY
+        self.rect.x += self.velocity_x
+        self.rect.y += self.velocity_y
+
+        if self.rect.bottom > SCREEN_HEIGHT - TILE_SIZE:
+            self.rect.bottom = SCREEN_HEIGHT - TILE_SIZE
+            self.velocity_y = 0
+            self.on_ground = True
+
+    def jump(self):
+        if self.on_ground:
+            self.velocity_y = JUMP_FORCE
+            self.on_ground = False
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, color):
+        super().__init__()
+        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.velocity_x = random.choice([-2, 2])
+
+    def update(self):
+        self.rect.x += self.velocity_x
+
+        if self.rect.right > SCREEN_WIDTH:
+            self.velocity_x = -2
+        elif self.rect.left < 0:
+            self.velocity_x = 2
+
+def draw_castle(screen):
+    castle_width = 200
+    castle_height = 300
+    castle_x = (SCREEN_WIDTH - castle_width) // 2
+    castle_y = SCREEN_HEIGHT - TILE_SIZE - castle_height
+
+    # Draw castle body
+    pygame.draw.rect(screen, BRICK_COLOR, (castle_x, castle_y, castle_width, castle_height))
+
+    # Draw castle windows
+    window_width = 30
+    window_height = 40
+    window_y = castle_y + 50
+    for i in range(3):
+        window_x = castle_x + (castle_width - window_width) // 2
+        pygame.draw.rect(screen, BLACK, (window_x, window_y + i * 80, window_width, window_height))
+
+    # Draw castle door
+    door_width = 60
+    door_height = 100
+    door_x = castle_x + (castle_width - door_width) // 2
+    door_y = castle_y + castle_height - door_height
+    pygame.draw.rect(screen, BLACK, (door_x, door_y, door_width, door_height))
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Super Mario FX")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.mario = Player(100, SCREEN_HEIGHT - TILE_SIZE * 2, RED)
+        self.luigi = Player(200, SCREEN_HEIGHT - TILE_SIZE * 2, GREEN)
+
+        self.enemies = pygame.sprite.Group()
+        self.enemies.add(Enemy(400, SCREEN_HEIGHT - TILE_SIZE, BLUE))
+        self.enemies.add(Enemy(600, SCREEN_HEIGHT - TILE_SIZE, BLUE))
+
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.mario)
+        self.all_sprites.add(self.luigi)
+        self.all_sprites.add(self.enemies)
+
+    def show_menu(self):
+        menu_font = pygame.font.Font(None, 50)
+        instructions_font = pygame.font.Font(None, 30)
+
+        title_text = menu_font.render("Super Mario FX", True, WHITE)
+        play_text = menu_font.render("Press ENTER to Play", True, WHITE)
+        exit_text = menu_font.render("Press ESC to Quit", True, WHITE)
+        controls_text = instructions_font.render("Controls: WASD for Luigi, Arrow Keys for Mario, Space to Jump", True, WHITE)
+
+        while True:
+            self.screen.fill(BLACK)
+            self.screen.blit(title_text, (SCREEN_WIDTH // 2 - title_text.get_width() // 2, 100))
+            self.screen.blit(play_text, (SCREEN_WIDTH // 2 - play_text.get_width() // 2, 200))
+            self.screen.blit(exit_text, (SCREEN_WIDTH // 2 - exit_text.get_width() // 2, 300))
+            self.screen.blit(controls_text, (SCREEN_WIDTH // 2 - controls_text.get_width() // 2, 400))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        return
+                    elif event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        exit()
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.mario.velocity_x = -PLAYER_SPEED
+        elif keys[pygame.K_RIGHT]:
+            self.mario.velocity_x = PLAYER_SPEED
+        else:
+            self.mario.velocity_x = 0
+
+        if keys[pygame.K_a]:
+            self.luigi.velocity_x = -PLAYER_SPEED
+        elif keys[pygame.K_d]:
+            self.luigi.velocity_x = PLAYER_SPEED
+        else:
+            self.luigi.velocity_x = 0
+
+        if keys[pygame.K_SPACE]:
+            self.mario.jump()
+        if keys[pygame.K_w]:
+            self.luigi.jump()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(WHITE)
+        pygame.draw.rect(self.screen, GREEN, (0, SCREEN_HEIGHT - TILE_SIZE, SCREEN_WIDTH, TILE_SIZE))
+        draw_castle(self.screen)
+        self.all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+    def run(self):
+        self.show_menu()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.handle_input() 
+            self.update()
+            self.draw()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
